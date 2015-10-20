ba34fee8bec84b8a0d4602a6d83ea5de10e56869
Tim Skaggs <tls.kaggs@gmail.com>
2015-10-20 00:30:12 -0500

    Add ui-router to grunt dependencies and created beginning routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,5 +15,21 @@ angular
     'ngMessages',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/main");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('main', {
+      url: "/main",
+      templateUrl: "main.html",
+      controller: 'MainCtrl'
+    });
+
+    // $locationProvider.html5Mode(true).hashPrefix('!');
+});
